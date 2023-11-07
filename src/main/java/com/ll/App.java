846717fc6dfdf468186a1d77a4980dbd659a0f58
leejaeyoon22 <tksska1514@gmail.com>
2023-11-07 16:57:35 +0900

    2단계 완료

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -1,8 +1,25 @@
 package com.ll;
 
+import java.util.Scanner;
+
 public class App {
     public static void run() {
+        Scanner sc = new Scanner(System.in);
+        String commend = "";
+        String saying = "";
+        String author = "";
+
         System.out.println("== 명언 앱 ==");
-        System.out.println("명령) 종료");
+
+        while(!(commend.equals("종료"))) {
+            System.out.print("명령) ");
+            commend = sc.nextLine();
+            if(commend.equals("등록")) {
+                System.out.print("명언 : ");
+                saying = sc.nextLine();
+                System.out.print("작가 : ");
+                author = sc.nextLine();
+            }
+        }
     }
 }
