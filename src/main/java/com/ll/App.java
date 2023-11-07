90d61fc50bd4a6b4bc1056a85b9082fbe2405bd5
leejaeyoon22 <tksska1514@gmail.com>
2023-11-07 17:31:41 +0900

    5단계 완료

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -1,5 +1,6 @@
 package com.ll;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class App {
@@ -9,6 +10,7 @@ public class App {
         String saying = "";
         String author = "";
         int order = 0;
+        ArrayList<SayingList> sayingRepository = new ArrayList<>();
 
         System.out.println("== 명언 앱 ==");
 
@@ -21,8 +23,18 @@ public class App {
                 System.out.print("작가 : ");
                 author = sc.nextLine();
                 order++;
+                
+                SayingList sayingList = new SayingList(order, saying, author);
+                sayingRepository.add(sayingList);
 
                 System.out.printf("%d번 명언이 등록되었습니다.\n", order);
+            } else if (commend.equals("목록")) {
+                System.out.println("번호 / 작가 / 명언");
+                System.out.println("----------------------");
+                for(int i=sayingRepository.size()-1; i>=0; i--) {
+                    SayingList sayingList = sayingRepository.get(i);
+                    System.out.printf("%d / %s / %s\n", sayingList.order, sayingList.author, sayingList.saying);
+                }
             }
         }
     }
---
+++ src/main/java/com/ll/SayingList.java
@@ -0,0 +1,12 @@
+package com.ll;
+
+public class SayingList {
+    int order;
+    String saying;
+    String author;
+    public SayingList(int order, String saying, String author) {
+        this.order = order;
+        this.saying = saying;
+        this.author = author;
+    }
+}
